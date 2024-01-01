c67159733cd134a4adda28ac52b62a38266d1387
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-01-01 17:15:43 -0300

    Obtendo a lista de continuar assistindo

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
 import { favoritesController } from "./controllers/favoritesController";
 import { likesController } from "./controllers/likesController";
+import { usersController } from "./controllers/usersController";
 
 const router = express.Router();
 
@@ -41,4 +42,5 @@ router.delete("/favorites", ensureAuth, favoritesController.delete);
 router.post("/likes", ensureAuth, likesController.save);
 router.delete("/likes", ensureAuth, likesController.delete);
 
+router.get("/users/current/watching", ensureAuth, usersController.watching);
 export { router };
