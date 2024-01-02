52c33178b04a4c7781c0f3a96443b63f95f08b41
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-01-02 15:57:36 -0300

    Obtendo informacoes da conta de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -42,5 +42,6 @@ router.delete("/favorites", ensureAuth, favoritesController.delete);
 router.post("/likes", ensureAuth, likesController.save);
 router.delete("/likes", ensureAuth, likesController.delete);
 
+router.get("/users/current", ensureAuth, usersController.show);
 router.get("/users/current/watching", ensureAuth, usersController.watching);
 export { router };
